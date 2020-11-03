7b96d9ea914986e098a3151dba4b1db300848561
Mathura <mathuradas333@gmail.com>
2020-11-03 09:25:31 +0530

    [Mathura]Add. Created database named 'address_book_service'

---
+++ AddressBookService.sql
@@ -0,0 +1,6 @@
+--creates new database
+create database address_book_service;
+--gives the names of all the databases
+SELECT name FROM sys.databases;
+--selects the payroll_service database for use
+use address_book_service;
