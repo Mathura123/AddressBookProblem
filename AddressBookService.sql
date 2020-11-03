bbe34bdd0cfdbab6fdddeeface83ac529aa2d0c6
Mathura <mathuradas333@gmail.com>
2020-11-03 09:44:09 +0530

    [Mathura]Add. Inserted values in address_book table

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -19,3 +19,11 @@ Email varchar(30) not null
 );
 --gives the info about the table named address_book
 select * from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'address_book';
+
+--Insert datas in table address_book
+insert into address_book values
+('Rakesh','Mehta','Lane 4','Mumbai','Maharashtra','489856','9999999999','rk123@gmail.com'),
+('Rahul','Kumar','K90/78 Allahabad','Allahabad','Uttar Pradesh','451207','8888888888','rahul777@gmail.com'),
+('Rekhi','Sharma','Street 56','Ajmer','Rajasthan','123456','8558585851','rekha@gmail.com');
+--retrives all datas in address_book
+select* from address_book;
